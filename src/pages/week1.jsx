26373c77d79522e4f2638a94156eb29a62acabeb
Daniel Liu <mliu192@se14l0120-08.ad.bcit.ca>
2025-10-20 22:43:15 -0700

    refine content for wk1blog

--- src/pages/week1.jsx
+++ src/pages/week1.jsx
@@ -1,84 +1,47 @@
 import styles from "./blogpost.module.css";
-import App from "../App.jsx";
+import "../App.css";
 import Header from "../ui/header/header.jsx";
+import TeamGrid from "../ui/TeamGrid/TeamGrid.jsx";
 
 export default function Week1() {
-    return (
-        <>
-            <Header />
-            <article className={styles.post}>
-                <figure className={styles.hero}>
-                    <img
-                        src='/blog-image1.jpg'
-                        alt='Project Idea Presentation'
-                    />
-                </figure>
+  return (
+    <>
+      <Header />
+      <article className={styles.post}>
+        <figure className={styles.hero}>
+          <img src="/wk1blog.jpg" alt="Project Idea Presentation" />
+        </figure>
 
-                <header className={styles.post_header}>
-                    <span className={styles.week}>Week 1</span>
-                    <h1 className={styles.title}>First Look at Forge</h1>
-                    <p className={styles.subtitle}>
-                        Forge Pitch Presentation and Team Formation
-                    </p>
-                </header>
+        <header className={styles.post_header}>
+          <span className={styles.week}>Week 1</span>
+          <h1 className={styles.title}>Project Idea Presentation</h1>
+          <p className={styles.subtitle}>September 14th, 2025</p>
+        </header>
 
-                <section className={styles.content}>
-                    <h2 className={styles.section_header}>
-                        Forge Pitch Presentation
-                    </h2>
-                    <p>
-                        This Fall semester on the 3rd of September. A pitch
-                        presentation took place whereby the forge team got to
-                        learn about all the amazing app ideas created by the
-                        Digital design and Development students and the
-                        Fullstack Development Students. We got the chance to
-                        pitch our app to some amazing well known industry
-                        professionals.
-                    </p>
-                    <p>
-                        A voting process took place whereby forge was selected
-                        as one of the final project ideas to be broken down and
-                        developed throughout the Fall 2025 semester.
-                    </p>
-                    <p>
-                        By the end of the presentations Forge had now 8 members
-                        , 5 Digital Design and Development students and 3
-                        Fullstack students.
-                    </p>
-                </section>
-                {/* <section className={styles.content}>
-                    <h2>Meet the Team:</h2>
-                    <p>Design</p>
-                    <div>
-                        <img src='/Camer.svg' />
-                        <img src='/Camer.svg' />
-                        <img src='/Camer.svg' />
-                        <img src='/Camer.svg' />
-                        <img src='/Camer.svg' />
-                    </div>
-                    <p>Developemnt</p>
-                    <div>
-                        <img src='/Camer.svg' />
-                        <img src='/Camer.svg' />
-                        <img src='/Camer.svg' />
-                    </div>
-                </section> */}
+        <section className={styles.content}>
+          <h3>The Beginning</h3>
+          <p>ConnectHER Hub is an organization formed in British Columbia, Canada. They host events and community gatherings that bring people together to share experiences, network, and learn from one another. In addition to fostering connections, they advocate for the representation of women in skilled trades and provide support for other underrepresented groups, helping to create a more inclusive and equitable industry. They've partnered with BCIT this year to work together in helping minorities in the skilled trades industry in BC.  </p>
+         <h3>The Project Pitch</h3>
+          <p>
+            This Fall semester on the 3rd of September, a pitch presentation took place
+            whereby the Forge team got to learn about all the amazing app ideas created by
+            the Digital Design and Development students and the Fullstack Web Development
+            students. We got the chance to pitch our app to some amazing well known
+            industry professionals.
+            <br />
+            <br />
+            A voting process took place whereby Forge was selected as one of the final
+            project ideas to be broken down and developed throughout the Fall 2025 semester.
+          </p>
 
-                <section className={styles.content}>
-                    <h2>Whats happening next week:</h2>
-                    <p>
-                        What an exciting Week for Forge ! We are so ready to
-                        start tackling the UX/UI side of forge and finally Build
-                        a fully fledged Application.{" "}
-                    </p>
-                    <ul>
-                        <li>First Team Meeting</li>
-                        <li>ConnectHer</li>
-                    </ul>
-                </section>
-
-                <p>Thanks for checking in ! See you back here next week ! </p>
-            </article>
-        </>
-    );
+          <h3>Meet the Team Behind Forge</h3>
+          <p>
+            Initially, the pitch team consisted of 4 members. By the end of the presentations, Forge had 8 members total — 5 Digital Design and Development students and 3 Fullstack students. Here are all the members!
+          </p>
+        <TeamGrid />
+        <p>It's a pleasure to meet you. Stay tuned for more updates!</p>
+        </section>
+      </article>
+    </>
+  );
 }
