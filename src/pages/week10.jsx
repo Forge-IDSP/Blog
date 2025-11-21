e616ed52ebbd2ba214778b013efe1d72a52d197e
CesariaMonforte <cesariajose24@gmail.com>
2025-11-20 22:27:44 -0800

    Text content Week 10 to 12

--- src/pages/week10.jsx
+++ src/pages/week10.jsx
@@ -11,30 +11,41 @@ export default function Week10() {
             <article className={styles.post}>
                 <figure className={styles.hero}>
                     <img
-                        src='/blog-image1.jpg'
+                        src='/week10.png'
                         alt='Week 10 Update'
                     />
                 </figure>
 
                 <header className={styles.post_header}>
                     <span className={styles.week}>Week 10</span>
-                    <h1 className={styles.title}>Lorem ipsum</h1>
-                    <p className={styles.subtitle}>Lorem ipsum</p>
+                    <h1 className={styles.title}>
+                        The Story Behind Forge and Fullstack Development
+                    </h1>
+                    <p className={styles.subtitle}>November 12th,2025</p>
                 </header>
 
                 <section className={styles.content}>
-                    <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum</p>
+                    <h2>Promotional Video Ideation</h2>
+                    <p>
+                        LAs we begin to see Forge become more and more
+                        accessible for users. The time has come to ideate a
+                        promotional video that highlights the key information we
+                        want our users to remember through visual storytelling.
+                        We began by creating a storyline for our promotional
+                        video that highlights the key reason we behind forge –
+                        to support students by providing a fun way of learning
+                        about careers in trades.
+                    </p>
                 </section>
 
                 <section className={styles.content}>
-                    <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum</p>
-                </section>
+                    <h2>Whats happening next week:</h2>
 
-                <section className={styles.content}>
-                    <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum</p>
+                    <ul>
+                        <li>Promotional video presentation and iteration</li>
+                        <li>Full Stack Development of AI Simulation</li>
+                    </ul>
+                    <p>Thanks for checking in! See you back here next week!</p>
                 </section>
             </article>
             <LatestPosts currentPostId='week10' />
--- src/pages/week12.jsx
+++ src/pages/week12.jsx
@@ -11,30 +11,59 @@ export default function Week12() {
             <article className={styles.post}>
                 <figure className={styles.hero}>
                     <img
-                        src='/blog-image1.jpg'
+                        src='/week12.jpeg'
                         alt='Week 12 Update'
                     />
                 </figure>
 
                 <header className={styles.post_header}>
                     <span className={styles.week}>Week 12</span>
-                    <h1 className={styles.title}>Lorem ipsum</h1>
-                    <p className={styles.subtitle}>Lorem ipsum</p>
+                    <h1 className={styles.title}>
+                        From a vision to a reality : Promotional Content
+                    </h1>
+                    <p className={styles.subtitle}>November 21st,2025</p>
                 </header>
 
                 <section className={styles.content}>
-                    <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum</p>
+                    <h2>Promotional Video Editing</h2>
+                    <p>
+                        Filming took place within a day whereby we gathered as a
+                        team to transform our vision into reality. We used a
+                        series of equipment to support in creating a scene such
+                        as classrooms ( for the high school student), mobile
+                        devices, protective gear such as helmets and much more.
+                    </p>
                 </section>
 
                 <section className={styles.content}>
-                    <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum</p>
+                    <h2>Printed Material Creation and Iteration</h2>
+                    <p>
+                        With the Final presentation slowly approaching we are
+                        creating a series of small visual elements to support
+                        our audience before we get to introduce them to forge at
+                        the Digital Design and Development annual showcase.
+                    </p>
                 </section>
 
                 <section className={styles.content}>
-                    <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum</p>
+                    <h2>Web Supplement Ideation</h2>
+                    <p>
+                        As our mobile Hi-fi prototype is finalised. It's time to
+                        provide a supplement to enhance our users' experience
+                        with forge. Based on our initial analysis we decided to
+                        focus on providing information on user progress such as
+                        badge achievement levels.
+                    </p>
+                </section>
+
+                <section className={styles.content}>
+                    <h2>Final Presentation Drafting</h2>
+                    <p>
+                        The question we have been asking ourselves is: “How do
+                        we present forge to the world in the most unique way
+                        possible?”. We decided to start a draft whereby we
+                        focused our structure on creating
+                    </p>
                 </section>
             </article>
             <LatestPosts currentPostId='week12' />
